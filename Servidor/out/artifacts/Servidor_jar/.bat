d00b3e987f12020b6a509904f587f113009fc6e3
Joel-Barboza <joelbp5631@gmail.com>
2025-11-20 16:51:50 -0600

    updating .bat to use %USERPROFILE% instead of absolute path

--- Servidor/out/artifacts/Servidor_jar/.bat
+++ Servidor/out/artifacts/Servidor_jar/.bat
@@ -1,2 +1,3 @@
-"C:\Users\Joel\.jdks\ms-17.0.17\bin\java.exe" -Dfile.encoding=UTF-8 -jar "%~dp0Servidor.jar"
+"%USERPROFILE%\.jdks\ms-17.0.17\bin\java.exe" -Dfile.encoding=UTF-8 -jar "%~dp0Servidor.jar"
 pause
+
